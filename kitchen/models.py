c993292d090cffea5b6ba011541e9968e3f84997
Soloveyk1n <swain.volt@gmail.com>
2023-11-22 13:37:04 +0200

    add completed admin

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField(null=True, blank=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes",)
+
+
