60ba4c011585c016e9779ec710d10feeef94849b
Soloveyk1n <swain.volt@gmail.com>
2023-12-11 15:21:58 +0200

    staticfiles managing

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 from dotenv import load_dotenv
 import os
 
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-e##8j%@3e*#6fi#&4rx+j
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -85,10 +87,21 @@ WSGI_APPLICATION = "restaurant_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Restaurant Production DB",
+        "USER": "zjaynxwt",
+        "PASSWORD": "63m6qvuGAlAER_z9lQTxH0KDTWfAk_eO",
+        "HOST": "cornelius.db.elephantsql.com",
+        "PORT": "5432",
     }
 }
+
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
@@ -129,7 +142,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 ASSETS_ROOT = "/static/assets"
 
