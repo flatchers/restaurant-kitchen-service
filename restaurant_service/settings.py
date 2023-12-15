9b95ae41d89af0a070d6905653580bfa656c48df
Soloveyk1n <swain.volt@gmail.com>
2023-12-15 12:34:34 +0200

    add restaurant-mate-lqjl.onrender.com to ALLOWED_HOSTS

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://restaurant-mate-lqjl.onrender.com"]
 
 
 # Application definition
